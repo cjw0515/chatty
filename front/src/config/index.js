b8dd5e61d0d49cb8982cb816e3a42d135c8ff786
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-03-14 17:03:07 +0900

    test

--- front/src/config/index.js
+++ front/src/config/index.js
@@ -1,12 +1,28 @@
 import firebase from 'firebase/app'
-import { develop } from './runtimeconfig.json'
-console.log(develop.apiKey)
+import { runtimeconfig } from './runtimeconfig.json'
+
+let apiKey, authDomain, databaseURL, storageBucket, projectId
+
+if(process.env.VUE_APP_ENV == "development"){
+  apiKey = process.env.VUE_APP_FIREBASE_APIKEY
+  authDomain = process.env.VUE_APP_AUTHDOMAIN
+  databaseURL = process.env.VUE_APP_DATABASEURL
+  projectId = process.env.VUE_APP_PROJECTID
+  storageBucket = process.env.VUE_APP_STORAGEBUCKET
+}else if(runtimeconfig){
+  apiKey = runtimeconfig.apikey
+  authDomain = runtimeconfig.authdomain
+  databaseURL = runtimeconfig.databaseurl
+  projectId = runtimeconfig.projectid
+  storageBucket = runtimeconfig.storagebucket
+}
+
 let firebaseAppConfig = {
-  apiKey: develop.apikey,
-  authDomain: develop.authdomain,
-  databaseURL: develop.databaseurl,
-  storageBucket: develop.apikey,
-  projectId: develop.projectid
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  storageBucket: storageBucket,
+  projectId: projectId
 };
 
 const firebaseApp = firebase.initializeApp(firebaseAppConfig);  
